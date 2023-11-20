d1261e0c2f0866bb998a2920d23c0652f496134b
Miroslav Hrabal <mhn@email.cz>
2023-11-20 23:56:23 +0100

    update setup.py url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="Bertalign",
     version="0.1.0",
-    url="https://github.com/bfsujason/bertalign",
+    url="https://github.com/hrabalm/bertalign",
     description="An automatic mulitlingual sentence aligner.",
     packages=find_packages(),
     install_requires=[],
