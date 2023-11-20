76b4cf64066e6921a34f7541dae985a366363c53
Miroslav Hrabal <mhn@email.cz>
2023-11-20 23:59:07 +0100

    specify dependencies in setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,10 @@ setup(
     url="https://github.com/hrabalm/bertalign",
     description="An automatic mulitlingual sentence aligner.",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "numba>=0.56.4",
+        "faiss-gpu>=1.7.2",
+        "sentence-splitter>=1.4",
+        "sentence-transformers>=2.2.2",
+    ],
 )
